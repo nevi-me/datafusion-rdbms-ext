882b996510543c4b89993677a82f8bec5da8c6f4
Wakahisa <nevilledips@gmail.com>
2022-06-18 10:57:02 +0200

    Move AST rewriter into a feature flag
    Disabled by default for now

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ use datafusion::{
     execution::{context::SessionState, runtime_env::*},
     prelude::*,
 };
+#[cfg(feature = "ast-rewrite")]
 use optimizer::*;
+#[cfg(feature = "ast-rewrite")]
 use physical_plan::SqlDatabaseQueryPlanner;
 
 mod catalog;
@@ -12,10 +14,10 @@ mod error;
 pub(crate) mod node;
 pub mod optimizer;
 mod parser;
-#[allow(dead_code, unused_variables)]
 pub mod physical_plan;
 pub mod sqldb;
 
+#[cfg(feature = "ast-rewrite")]
 /// Create a [SessionContext] with RDBMS planners and optimizers
 pub fn make_rdbms_context() -> SessionContext {
     let config = SessionConfig::new().with_information_schema(true);
@@ -25,6 +27,18 @@ pub fn make_rdbms_context() -> SessionContext {
     )
     .with_query_planner(Arc::new(SqlDatabaseQueryPlanner {}))
     .add_optimizer_rule(Arc::new(QueryPushdownOptimizerRule {}));
+
+    SessionContext::with_state(state)
+}
+
+#[cfg(not(feature = "ast-rewrite"))]
+/// Create a [SessionContext] with no AST planner
+pub fn make_rdbms_context() -> SessionContext {
+    let config = SessionConfig::new().with_information_schema(true);
+    let state = SessionState::with_config_rt(
+        config,
+        Arc::new(RuntimeEnv::new(RuntimeConfig::default()).unwrap()),
+    );
 
     SessionContext::with_state(state)
 }
